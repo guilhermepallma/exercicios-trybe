207ef87d7ed304deb011d01b43f7f6c54855b782
Guilherme Palma <guilhermepallma@gmail.com>
2022-02-12 18:12:10 -0400

    Exercicio-02 do bloco/4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -2,7 +2,15 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //Exercício 01
 
-for (let index = 0; index < numbers.length; index +=1) {
-  console.log(numbers[index]);
-}
+// for (let index = 0; index < numbers.length; index +=1) {
+//   console.log(numbers[index]);
+// }
 
+//Exercício 02
+
+let soma = 0;
+
+for (let index = 0; index < numbers.length; index +=1) {
+  soma += numbers[index]
+}
+console.log(soma);
