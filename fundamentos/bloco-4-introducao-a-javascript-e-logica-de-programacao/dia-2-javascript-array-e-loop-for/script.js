fcf8d6d4aa5f93e53054df27377c4cff9d34457c
Guilherme Palma <guilhermepallma@gmail.com>
2022-02-12 21:01:25 -0400

    Exercicio-04 do bloco/4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -16,7 +16,21 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // console.log(sum);
 
-//Exercicío 03
+//Exercício 03
+
+// let sum = 0;
+// let result = 0;
+// let average = 0;
+
+
+// for (let index = 0; index < numbers.length; index +=1) {
+//   result = sum += numbers[index];
+//   average = result / numbers.length;
+// }
+
+// console.log(average);
+
+//Exercício 04
 
 let sum = 0;
 let result = 0;
@@ -27,5 +41,8 @@ for (let index = 0; index < numbers.length; index +=1) {
   result = sum += numbers[index];
   average = result / numbers.length;
 }
-
-console.log(average);
+  if(average > 20) {
+  console.log('valor maior que 20');
+} else {
+  console.log('valor menor ou igual a 20');
+}
