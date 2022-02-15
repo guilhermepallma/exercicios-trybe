ef7c898a72612c563de37404018c8efb543e4258
Guilherme Palma <guilhermepallma@gmail.com>
2022-02-15 00:17:44 -0400

    Exercicio-07 do bloco/4.2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -60,14 +60,25 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 //Exercício 06
 
-let reapet = 0;
+// let reapet = 0;
+
+// for (let i = 0; i < numbers.length; i += 1){
+//   if (numbers[i] % 2 !== 0) {
+//     reapet += 1;
+//     console.log(reapet);
+//   } else if (reapet === 0) {
+//     console.log('nenhum valor ímpar encontrado');
+//     break;
+//   }
+// }
+
+// Exercício 07
+
+let larger = numbers[0];
 
 for (let i = 0; i < numbers.length; i += 1){
-  if (numbers[i] % 2 !== 0) {
-    reapet += 1;
-    console.log(reapet);
-  } else if (reapet === 0) {
-    console.log('nenhum valor ímpar encontrado');
-    break;
+  if (larger > numbers[i]) {
+      larger = numbers[i];
   }
 }
+console.log(larger);
